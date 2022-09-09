786ee8f7f1b68229a77ee1737ab17279b728ae71
Kareem Elsafy <73001284+kareem207@users.noreply.github.com>
2022-09-09 18:55:59 +0200

    Create App.js

---
+++ Tenzies Game/App.js
@@ -0,0 +1,11 @@
+import MainSection from "./components/MainSection";
+
+function App(props) {
+    return (
+        <div>
+            <MainSection />
+        </div>
+    );
+}
+
+export default App;
